43e8144277b15af0a006c0de377b55264736bed3
David-Allen <davida.asencio@gmail.com>
2022-05-17 18:40:16 -0400

    took me a minute but i remembered how to make the twosum pairs unique

--- skeleton/phase_1_arrays.js
+++ skeleton/phase_1_arrays.js
@@ -8,4 +8,19 @@ Array.prototype.uniq = function() {
     return res; 
 };
 
-console.log([1,1,2,2,3,3,4,4].uniq());
+// console.log([1,1,2,2,3,3,4,4].uniq());
+
+
+Array.prototype.twoSum = function() {
+    let res = []
+    for (let i = 0; i < this.length; i++) {
+        for (let j = i; j < this.length; j ++) {
+            if (this[i] + this[j] === 0) {
+                res.push([this[i], this[j]])
+            }   
+        }
+    }
+    return res;
+}
+
+console.log([1,2,3,-2,-1].twoSum());
